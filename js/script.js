03d78ee27d53adea7cbe27aac5293c30fee2d411
dmkirshon <kirshon.david@gmail.com>
2022-01-21 17:27:07 +0200

    feat: Add new book submissions through a UI form

--- js/script.js
+++ js/script.js
@@ -3,13 +3,17 @@
  * Author: David Kirshon
  */
 
+const newBookBtn = document.querySelector('.book-new');
+const newBookForm = document.querySelector('.book-form');
+const newBookSubmissionMessage = document.querySelector('.book-submission-message');
+
 let myLibrary = [];
 
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = read ? 'been read already':'not been read yet';
+    this.read = read ? 'already been read':'not been read yet';
 }
 
 Book.prototype.bookInfo = function() {
@@ -29,3 +33,30 @@ function displayBooks() {
 const hobbitBook = new Book('The Hobbit', 'J.R.R. Tolkien', 295, true);
 addBookToLibrary(hobbitBook);
 displayBooks();
+
+// add book button
+
+newBookBtn.addEventListener('click', showBookForm);
+
+function showBookForm() {
+    newBookSubmissionMessage.hidden = true;
+    newBookForm.hidden = false;
+}
+
+newBookForm.addEventListener('submit', submitForm);
+
+function submitForm(event) {
+    event.preventDefault();
+
+    const bookTitle = document.getElementById('bookTitle').value;
+    const bookAuthor = document.getElementById('bookAuthor').value;
+    const bookPages = document.getElementById('bookPages').value;
+    const bookRead = document.getElementById('bookRead').checked;
+
+    const newBookSubmission = new Book(bookTitle, bookAuthor, bookPages, bookRead);
+
+    addBookToLibrary(newBookSubmission);
+    newBookForm.reset();
+    newBookForm.hidden = true;
+    newBookSubmissionMessage.hidden = false;
+}
